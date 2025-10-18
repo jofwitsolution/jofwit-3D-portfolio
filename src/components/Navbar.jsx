7b900c6ee7f5ae1c2c83369a68d02c66cc1ddc99
Oluwafemi Joseph Faleye <faleyeoluwafemi1@gmail.com>
2025-10-18 20:42:33 +0100

    update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,7 +43,7 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer flex ">
+          <p className="text-[#915EFF] text-[18px] font-bold cursor-pointer flex ">
             Bitnox Solution &nbsp;
           </p>
         </Link>
